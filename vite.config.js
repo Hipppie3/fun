8503d049cf14f3f40b2fbe5263493485b6ada9b3
Hipppie3 <107952593+Hipppie3@users.noreply.github.com>
2024-11-25 16:47:59 -0800

    test

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
 export default defineConfig({
-  plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5111', // Backend URL
+        target: 'https://855c-172-249-48-107.ngrok-free.app', // Use your ngrok URL here
         changeOrigin: true,
         secure: false,
       },
